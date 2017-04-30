14b8788798ddfd88212f4e781d904fed84c16a8a
yl-L <sunner648@163.com>
2017-04-30 12:24:08 +0800

    Update limit.py

--- Second-hand housing/limit.py
+++ Second-hand housing/limit.py
@@ -3,39 +3,27 @@ import urllib, gzip, traceback, re
 from time import ctime, sleep
 import subprocess
 from threading import Lock, current_thread, Thread
-
-
-def get_urldata(main_url):
-    try:
-        child = subprocess.Popen(['python', '1.py', "%s"%main_url])
-        child.wait(15)
-    except Exception as e:
-        child.kill()
-        print('获取超时，重新获取')
-        get_urldata(main_url)
-    with open("./cache.txt", encoding='gbk')as f:
-        a = ''.join(f.readlines())
-    return a
+from urllib import request
 
 
 def limit_area(main_url, url, low, high):
     print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!进入limit_area')
     url_list = []
     page_list = []
-    data = get_urldata(url)
+    data = get_urldata(url,error_num=0, must=1)
     url_info = url.split('/')[-2]
     print('这是',url_info)
     l = re.findall(r'<p id="shangQuancontain" class="contain">([\s,\S]*?)</p>', data)
     partition_area = re.findall(r'"(/%s-b.*?)" >.{1,5}</a>' % url_info, l[0].replace('  class="org bold"',''))
     detail_url = list(map(lambda x:main_url + x, partition_area))
     for url1 in detail_url:
-        data1 = get_urldata(url1)
+        data1 = get_urldata(url1,error_num=0, must=1)
         if not re.findall(r'很抱歉，没有找到与', data1):
             try:
                 page = int(re.findall(r'"txt">共([0-9]{1,4})页</span>', data1)[0])
             except IndexError:
                 sleep(2)
-                data1 = get_urldata(url1)
+                data1 = get_urldata(url1,error_num=0, must=1)
                 page = int(re.findall(r'"txt">共([0-9]{1,4})页</span>', data1)[0])
             print(url1)
             print('这是page', page)
@@ -55,7 +43,7 @@ def add_limit(url, url_list, page_list, low, high):
     for i in limit:
         detail_url = url+i
         print(detail_url)
-        data = get_urldata(detail_url)
+        data = get_urldata(detail_url,error_num=0, must=1)
         if not re.findall(r'很抱歉，没有找到与', data):
             page = int(re.findall(r'"txt">共([0-9]{1,4})页</span>', data)[0])
             print(page)
@@ -77,7 +65,7 @@ def add_more_limit(url, url_list, page_list, low, high):
     if high < 250:
         detail_url = url + '-j2%s-k2%s' % (low, high)
         print(detail_url)
-        data = get_urldata(detail_url)
+        data = get_urldata(detail_url,error_num=0, must=1)
         if not re.findall(r'很抱歉，没有找到与', data):
             page = int(re.findall(r'"txt">共([0-9]{1,4})页</span>', data)[0])
             print(page)
@@ -99,7 +87,7 @@ def add_more_limit(url, url_list, page_list, low, high):
     else:
         detail_url = url + '-j2%s' % high
         print(detail_url)
-        data = get_urldata(detail_url)
+        data = get_urldata(detail_url,error_num=0, must=1)
         if not re.findall(r'很抱歉，没有找到与', data):
             page = int(re.findall(r'"txt">共([0-9]{1,4})页</span>', data)[0])
             url_list.append(detail_url)
@@ -107,5 +95,74 @@ def add_more_limit(url, url_list, page_list, low, high):
         else:
             print('该页没有房源信息，不添加')
     return url_list, page_list
+
+
+def get_urldata(main_url,error_num,must):#获取网页信息
+    print(ctime(), '这是url', main_url)
+    req = urllib.request.Request(main_url)
+    req.add_header('User-Agent',
+                   'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1) ; 360SE)')
+    try:#网页压缩的情况下使用
+        print('!!!!!!!!!!!!!!!!!!开始try!!!!!!!!!!!!!!!!!!')
+        data = request.urlopen(req,timeout=10).read()
+        data = gzip.decompress(data).decode("gb18030")
+        if re.findall('二手房', data) and re.findall('body', data) and re.findall('fang.com', data) and isinstance(data,str):
+            print('获取正常')
+            pass
+        else:
+            get_urldata(main_url, error_num, must)
+        print('结束get_urldata')
+        return data
+    except TimeoutError:
+        print('!!!!!!!!!!!!!!TimeoutError!!!!!!!!!!!!!!!!!!!!!!')
+        sleep(1)
+        print('访问请求失败，睡眠1s')
+        if must == 1:
+            get_urldata(main_url, error_num, must)
+        else:
+            if error_num < 8:
+                error_num += 1
+                get_urldata(main_url, error_num, must)
+            else:
+                data = ''
+                return data
+    except OSError as e:#网页未压缩的情况下使用
+        print('!!!!!!!!!!!!!!超时!!!!!!!!!!!!!!!!!!!!!!')
+        print(e)
+        try:
+            data = request.urlopen(req, timeout=10).read().decode('gb18030')
+            return data
+        except Exception:
+            if must == 2:
+                data = 0
+                return data
+            if must == 1:
+                get_urldata(main_url, error_num, must)
+            else:
+                if error_num < 8:
+                    error_num += 1
+                    get_urldata(main_url, error_num, must)
+                else:
+                    data = ''
+                    return data
+    except UnicodeEncodeError:
+        print('!!!!!!!!!!!!!!!!!!!写入文件错误，自动跳过!!!!!!!!!!!!!!!!!!!!!!')
+        data = ''
+        return data
+    except Exception as e:
+        print('!!!!!!!!!!!!!!Exception!!!!!!!!!!!!!!!!!!!!!')
+        print(traceback.format_exc())
+        sleep(1)
+        print('访问请求失败，睡眠1s')
+        if must == 1:
+            get_urldata(main_url, error_num, must)
+        else:
+            if error_num < 8:
+                error_num += 1
+                get_urldata(main_url, error_num, must)
+            else:
+                data = ''
+                return data
+
 # g21,g22,g23,g24,g299
 # 'a573','-a574','-a575','-a576','-a577','-a578','-a579','-a580','-a5120'
